864b1acae109fb2582a27ab6523a2191df7b6cea
prubiera85 <paula.rubiera@oneclick.es>
2025-07-16 12:42:49 +0200

    feat: tests

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,21 +1,23 @@
-import React from 'react';
-import './Button.scss';
+import React from "react";
+import "./Button.scss";
 
 // variant: primary, secondary, outline, transparent, danger, success
-// size: small, medium, large 
+// size: small, medium, large
 // disabled: boolean
 // loading: boolean
 
+const Button = ({
+  children,
+  variant = "primary",
+  size = "medium",
+  disabled = false,
+  loading = false,
+}) => {
+  const buttonClass = `button button--${variant} button--${size} ${
+    disabled ? "button--disabled" : ""
+  } ${loading ? "button--loading" : ""}`;
 
-const Button = ({ children,  variant = 'primary', size = 'medium', disabled = false, loading = false}) => {
-
-    const buttonClass = `button button--${variant} button--${size} ${disabled ? 'button--disabled' : ''} ${loading ? 'button--loading' : ''}`;
-
-  return (
-    <button className={buttonClass} >
-      {children}
-    </button>
-  );
+  return <button className={buttonClass}>{children}</button>;
 };
 
-export default Button; 
+export default Button;
